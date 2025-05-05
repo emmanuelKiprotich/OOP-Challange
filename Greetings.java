351100637aae2d0ff6f038f288878a2a5c6e157b
mattmanu <emmanuelmatthews77@gmail.com>
2025-05-05 14:52:52 +0300

    Finished Greetings.java

--- Greetings.java
+++ Greetings.java
@@ -1,11 +1,13 @@
 public class Greetings {
-    // This is a simple Java program that prints "Hi there" to the console.
-    void greet() {
-        System.out.println("Hi there");
+        // This is a simple Java program that prints "Hi there" to the console.
+        void greet() {
+            System.out.println("Hi there");
+        }
+        public static void main(String[] args) {
+            Greetings greetings = new Greetings();
+            greetings.greet();
+        }
+        
     }
-    public static void main(String[] args) {
-        Greetings greetings = new Greetings();
-        greetings.greet();
-    }
-    
-}
+
+
